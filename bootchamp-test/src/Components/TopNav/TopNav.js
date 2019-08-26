ddc6dbaa1f5546584902db0ae3d318629d832514
kirbycampbell <jkirbycampbell@gmail.com>
2019-08-26 14:39:18 -0700

    NavBar Fixed Up

--- bootchamp-test/src/Components/TopNav/TopNav.js
+++ bootchamp-test/src/Components/TopNav/TopNav.js
@@ -4,7 +4,15 @@ import "./TopNav.css";
 const TopNav = () => {
   return (
     <div className="Outer-TopNav">
-      <div className="Inner-TopNav">Heyy</div>
+      <div className="Inner-TopNav">
+        <div className="Nav-Grps-Outer">
+          <div className="Nav-Grps">BootChamp</div>
+          <div className="Nav-Grps">Topics</div>
+          <div className="Nav-Grps">Cities</div>
+          <div className="Nav-Grps">Resources</div>
+          <div className="Nav-Grps">Profile</div>
+        </div>
+      </div>
     </div>
   );
 };
