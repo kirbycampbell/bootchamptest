ad1803b91c3eb2cc8ef5c5fa207c4bf5b332925e
merestrickland <mere.strickland@gmail.com>
2019-08-30 17:08:42 -0400

    started cities page

---
+++ bootchamp-test/src/Components/Cities/Cities.data.js
@@ -0,0 +1,44 @@
+const CITY_DATA = [
+    {
+        name: 'Portland',
+        state: 'Oregon',
+        meetupList: [],
+        members: [],
+        resources: [],
+        localTopics: []
+    },
+    {
+        name: 'New Orleans',
+        state: 'Louisiana',
+        meetupList: [],
+        members: [],
+        resources: [],
+        localTopics: []
+    },
+    {
+        name: 'New York City',
+        state: 'New York',
+        meetupList: [],
+        members: [],
+        resources: [],
+        localTopics: []
+    },
+    {
+        name: 'Madison',
+        state: 'Wisconsin',
+        meetupList: [],
+        members: [],
+        resources: [],
+        localTopics: []
+    },
+    {
+        name: 'Philadelphia',
+        state: 'Pennslyvania',
+        meetupList: [],
+        members: [],
+        resources: [],
+        localTopics: []
+    }
+]
+
+export default CITY_DATA
--- bootchamp-test/src/Components/Cities/Cities.js
+++ bootchamp-test/src/Components/Cities/Cities.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Cities.css";
+import CITY_DATA from './Cities.data'
 
 const Cities = () => {
-  return <div>Cities</div>;
-};
+
+  const [cities, setCities] = useState(CITY_DATA);
+
+  useEffect(() => {
+    setCities(CITY_DATA);
+  }, []);
+
+  return (
+    <div>
+      {cities.map(city => {
+        return <div key={city}>{city.name}</div>;
+      })}
+    </div>
+  );
+
+}
+
 export default Cities;
