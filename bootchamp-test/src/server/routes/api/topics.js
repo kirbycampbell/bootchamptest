d5224c5ba8fa0c92ca331fdd437ade11343bb6f7
kirbycampbell <jkirbycampbell@gmail.com>
2019-09-01 20:27:41 -0700

    Update topics.js

--- bootchamp-test/src/server/routes/api/topics.js
+++ bootchamp-test/src/server/routes/api/topics.js
@@ -1,121 +1,121 @@
-const express = require ('express');
-const mongodb = require ('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
-const router = express.Router ();
+const router = express.Router();
 
 // get topics -
-router.get ('/', async (req, res) => {
-  const topics = await loadTopics ();
-  res.send (await topics.find ({}).toArray ());
+router.get("/", async (req, res) => {
+  const topics = await loadTopics();
+  res.send(await topics.find({}).toArray());
 });
 
 // !
-router.get ('/:id', async (req, res) => {
-  const topics = await loadTopics ();
-  res.send (
-    await topics.findOne ({
-      id: req.params.id,
+router.get("/:id", async (req, res) => {
+  const topics = await loadTopics();
+  res.send(
+    await topics.findOne({
+      id: req.params.id
     })
   );
 });
 
 // !
-router.get ('/usertopics/:id', async (req, res) => {
-  const topics = await loadTopics ();
-  res.send (
+router.get("/usertopics/:id", async (req, res) => {
+  const topics = await loadTopics();
+  res.send(
     await topics
-      .find ({
-        createdBy: req.params.id,
+      .find({
+        createdBy: req.params.id
       })
-      .toArray ()
+      .toArray()
   );
 });
 
 // add topics - !
-router.post ('/', async (req, res) => {
-  const topics = await loadTopics ();
-  await topics.insertOne ({
+router.post("/", async (req, res) => {
+  const topics = await loadTopics();
+  await topics.insertOne({
     id: req.body.id,
     name: req.body.name,
-    content: {images: [''], link: '', code: '', video: '', text: ''},
-    createdAt: new Date (),
-    updatedAt: new Date (),
+    content: { images: [""], link: "", code: "", video: "", text: "" },
+    createdAt: new Date(),
+    updatedAt: new Date(),
     createdBy: req.body.createdBy,
     likedBy: [],
     cities: [],
-    tags: [],
+    tags: []
   });
-  res.status (201).send (
-    await topics.findOne ({
-      id: req.body.id,
+  res.status(201).send(
+    await topics.findOne({
+      id: req.body.id
     })
   );
 });
 
-router.patch ('/:id', async (req, res) => {
-  const topics = await loadTopics ();
-  await topics.updateOne (
+router.patch("/:id", async (req, res) => {
+  const topics = await loadTopics();
+  await topics.updateOne(
     {
-      id: req.params.id,
+      id: req.params.id
     },
     {
       $set: {
         name: req.body.name,
         likedBy: req.body.likedBy,
-        //updatedAt: new Date (),
+        updatedAt: new Date(),
         tags: req.body.tags,
         cities: req.body.cities,
-        content: req.body.content,
-      },
+        content: req.body.content
+      }
     }
   );
-  res.status (201).send (
-    await topics.findOne ({
-      id: req.params.id,
+  res.status(201).send(
+    await topics.findOne({
+      id: req.params.id
     })
   );
 });
 
-router.patch ('/like/:id', async (req, res) => {
-  const topics = await loadTopics ();
-  let topicLike = await topics.findOne ({
-    id: req.params.id,
+router.patch("/like/:id", async (req, res) => {
+  const topics = await loadTopics();
+  let topicLike = await topics.findOne({
+    id: req.params.id
   });
 
-  topicLike.likedBy.push ('NewLike');
-  await topics.updateOne (
+  topicLike.likedBy.push("NewLike");
+  await topics.updateOne(
     {
-      id: req.params.id,
+      id: req.params.id
     },
     {
       $set: {
-        likedBy: topicLike.likedBy,
-      },
+        likedBy: topicLike.likedBy
+      }
     }
   );
-  res.status (201).send (
-    await topics.findOne ({
-      id: req.params.id,
+  res.status(201).send(
+    await topics.findOne({
+      id: req.params.id
     })
   );
 });
 
 // delete topics - !
-router.delete ('/:id', async (req, res) => {
-  const topics = await loadTopics ();
-  await topics.deleteOne ({id: req.params.id});
-  res.status (200).send ();
+router.delete("/:id", async (req, res) => {
+  const topics = await loadTopics();
+  await topics.deleteOne({ id: req.params.id });
+  res.status(200).send();
 });
 
 //loadPostsCollection from MongoDB
-async function loadTopics () {
-  const client = await mongodb.MongoClient.connect (
-    'mongodb://bootchampAdmin:adminBootchamp1@ds355357.mlab.com:55357/bootchamp',
+async function loadTopics() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb://bootchampAdmin:adminBootchamp1@ds355357.mlab.com:55357/bootchamp",
     {
-      useNewUrlParser: true,
+      useNewUrlParser: true
     }
   );
-  return client.db ('bootchamp').collection ('topics');
+  return client.db("bootchamp").collection("topics");
 }
 
 module.exports = router;
