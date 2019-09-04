8c428daf7c037311a0f1706d120532e085f82fc7
kirbycampbell <jkirbycampbell@gmail.com>
2019-09-03 17:47:03 -0700

    Tags Finished

--- bootchamp-test/src/server/routes/api/tags.js
+++ bootchamp-test/src/server/routes/api/tags.js
@@ -3,13 +3,13 @@ const mongodb = require("mongodb");
 const router = express.Router();
 const { loadTags } = require("./databases");
 
-// get tags -
+// 1). Query all Tags:
 router.get("/", async (req, res) => {
   const tags = await loadTags();
   res.send(await tags.find({}).toArray());
 });
 
-// !
+// 2). Get Specific Tag by ID
 router.get("/:id", async (req, res) => {
   const tags = await loadTags();
   res.send(
@@ -19,28 +19,30 @@ router.get("/:id", async (req, res) => {
   );
 });
 
-router.get("/search/:name", async (req, res) => {
+// 3). Search Tags for exact label match:
+router.get("/search/:label", async (req, res) => {
   const tags = await loadTags();
   res.send(
     await tags.findOne({
-      label: req.params.name
+      label: req.params.label
     })
   );
 });
 
-router.get("/matches/:name", async (req, res) => {
+// 4). Search Tags for part label match (while typing):
+router.get("/matches/:label", async (req, res) => {
   const tags = await loadTags();
-  let term = req.params.name.toLowerCase();
+  let term = req.params.label.toLowerCase();
   res.send(
     await tags
       .find({
-        label: { $regex: new RegExp(req.params.name) }
+        label: { $regex: new RegExp(term) }
       })
       .toArray()
   );
 });
 
-// add tags - !
+// 5). Create new Tag - POST
 router.post("/", async (req, res) => {
   const tags = await loadTags();
   await tags.insertOne({
@@ -54,11 +56,11 @@ router.post("/", async (req, res) => {
   );
 });
 
-// delete tags - !
+// 6). Delete Specific Tag by ID
 router.delete("/:id", async (req, res) => {
   const tags = await loadTags();
   await tags.deleteOne({ id: req.params.id });
-  res.status(200).send();
+  res.status(200).send(true);
 });
 
 module.exports = router;
