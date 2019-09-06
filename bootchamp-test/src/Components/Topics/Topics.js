33a7aa021aaaddb75bf5f6a395e00352288795ee
kirbycampbell <jkirbycampbell@gmail.com>
2019-09-06 14:41:19 -0700

    Fixed Up Topic Stuff as example

--- bootchamp-test/src/Components/Topics/Topics.js
+++ bootchamp-test/src/Components/Topics/Topics.js
@@ -1,28 +1,31 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import "./Topics.scss";
-import Tile from '../Tile/tile.component'
-import { URL } from '../../constants/url'
+import Tile from "../Tile/tile.component";
+import { URL } from "../../constants/url";
 
-const axios = require('axios')
+const axios = require("axios");
 
+const Topics = ({ id, name, content, tags }) => {
+  const [topics, setTopics] = useState([]);
 
-const Topics = ({ id, name, content, tags}) => {
-
-  axios
-    .get(URL + 'topics/')
-    .then(function (response) {
-      let topics = response.data
-      console.log(topics)
-    })
+  useEffect(() => {
+    axios.get(URL + "topics/").then(function(response) {
+      setTopics(response.data);
+    });
+  }, []);
+  console.log(topics);
 
   return (
-
-  <div className='topics'>
-    {console.log(topics)}
-    <Tile />
-  </div>
-  )
-
+    <div className="topics">
+      {topics.map(topic => (
+        <div className="Outer-Topic" key={topic.id}>
+          <h2>{topic.name}</h2>
+          <div>{topic.content.text}}</div>
+          <div>Created At: {topic.createdAt}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Topics;
