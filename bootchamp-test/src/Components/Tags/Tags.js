ca415c82b779a688e190a8c9e029c72c8410f4c3
kirbycampbell <jkirbycampbell@gmail.com>
2019-09-13 10:50:41 -0700

    Tag Search, Select, and Filter Added

--- bootchamp-test/src/Components/Tags/Tags.js
+++ bootchamp-test/src/Components/Tags/Tags.js
@@ -7,31 +7,85 @@ const axios = require("axios");
 const Tags = () => {
   const [tags, setTags] = useState([]);
   const [typeTag, setTypeTag] = useState("");
+  const [queryMatch, setQueryMatch] = useState([]);
+  const [queryList, setQueryList] = useState([]);
   const user = useSelector(state => state.user);
 
   useEffect(() => {
     axios.get(URL + "tags/").then(function(response) {
-      console.log(response);
       setTags(response.data);
     });
   }, []);
+
+  useEffect(() => {
+    if (typeTag.length > 2) {
+      axios.get(URL + "tags/matches/" + typeTag).then(function(response) {
+        console.log(response.data);
+        setQueryMatch(response.data);
+      });
+    }
+  }, [typeTag]);
 
   const handleTyping = e => {
     setTypeTag(e.target.value);
   };
 
+  const handleSelect = tag => {
+    let newList = queryList.filter(item => {
+      return item.id !== tag.id;
+    });
+    newList.push(tag);
+    setQueryList(newList);
+    setTypeTag("");
+    setQueryMatch([]);
+  };
+
+  const removeTag = tag => {
+    let newList = queryList.filter(item => {
+      return item.id !== tag.id;
+    });
+    setQueryList(newList);
+  };
+
   return (
     <div>
       <div>
-        <input
-          className="tag-card"
-          type="text"
-          name="tag"
-          placeholder="Type Tag Name here"
-          onChange={handleTyping}
-          value={typeTag}
-        />
+        <div className="tag-card">
+          {/* ::::::::::: Chosen Tags :::::::::::::: */}
+          {queryList.length > 0 &&
+            queryList.map(item => {
+              return (
+                <span key={item.id} className="chosen-tag">
+                  {item.label}
+                  <div className="x-out" onClick={() => removeTag(item)}>
+                    X
+                  </div>
+                </span>
+              );
+            })}
+          {/* ::::::::::: Input Section :::::::::::::: */}
+          <input
+            type="text"
+            name="tag"
+            placeholder="Type Tag Name here"
+            onChange={handleTyping}
+            value={typeTag}
+          />
+        </div>
+        {/* ::::::::::: List of Search Matches :::::::::::::: */}
+        {queryMatch.length > 0 && (
+          <div>
+            {queryMatch.map(qTag => {
+              return (
+                <div key={qTag.id} onClick={() => handleSelect(qTag)}>
+                  {qTag.label}
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
+      {/* ::::::::::: List of All Tags :::::::::::::: */}
       <div>
         {tags.map(tag => {
           return (
